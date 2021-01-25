da0f19ac99f4d7c2e8ed1c7c3673b4f388733b3f
Kathryn Hunter <kathryn.hunter@pcc.edu>
2021-01-25 13:51:40 -0800

    Added HelloWorld class for testing purposes

--- src/edu/pcc/cis/conversion/HelloWorld.java
+++ src/edu/pcc/cis/conversion/HelloWorld.java
@@ -1,4 +1,10 @@
 package edu.pcc.cis.conversion;
 
 public class HelloWorld {
+    public HelloWorld() {
+    }
+
+    public void printStatement() {
+        System.out.println("Hello world.");
+    }
 }
